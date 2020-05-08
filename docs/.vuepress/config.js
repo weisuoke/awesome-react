8d34490a20d240509c55ed1e45c9c132b8bcf446
梅林的法杖 <530264127@qq.com>
2020-05-08 18:03:22 +0800

    feat: 🎸 first commit

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  dest: 'dist',
+  title: 'Awesome react',
+  themeConfig: {
+    editLinks: false,
+    docsDir: 'docs',
+    search: false,
+    lastUpdated: "更新时间",
+    nav: [],
+    sidebar: [
+    ]
+  }
+}
